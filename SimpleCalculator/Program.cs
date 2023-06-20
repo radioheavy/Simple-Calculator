71f10447979f83590562bc08b88d7abf710cce91
oktay <oktay@DESKTOP-UUAHGJ8>
2023-06-20 19:31:17 +0300

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PerformCalculation();
+            }
+        }
+
+        static void PerformCalculation()
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("Result: " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("Result: " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("Result: " + (num1 * num2));
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine("Result: " + (num1 / num2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! Division by zero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+            }
+        }
+    }
+}
